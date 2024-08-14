affec390d50d6b2da2dd3da4241cd84da5e78e3f
Zach <zach@coinculture.info>
2024-08-14 17:52:55 -0400

    create bond and set name (#1)
    * create bond and set name
    
    fixes
    
    * rm script

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-RECORD_FILE=tmp.rf.$$
+AR_RECORD_FILE=tmp.rf.$$
+ADR_RECORD_FILE=tmp.rf.$$
 CONFIG_FILE=`mktemp`
 
 CERC_APP_TYPE=${CERC_APP_TYPE:-"webapp"}
@@ -29,6 +30,7 @@ EOF
 
 if [ -z "$CERC_REGISTRY_BOND_ID" ]; then
   bond_id=$(laconic -c $CONFIG_FILE registry bond create --type alnt --quantity 1000000000000 --user-key "${CERC_REGISTRY_USER_KEY}")
+
   CERC_REGISTRY_BOND_ID=$(echo ${bond_id} | jq -r .bondId)
 fi
 
@@ -38,7 +40,7 @@ if [ -z "$next_ver" ] || [ "1" == "$next_ver" ]; then
   next_ver=0.0.1
 fi
 
-cat <<EOF | sed '/.*: ""$/d' > "$RECORD_FILE"
+cat <<EOF | sed '/.*: ""$/d' > "$AR_RECORD_FILE"
 record:
   type: ApplicationRecord
   version: ${next_ver}
@@ -55,22 +57,50 @@ record:
 EOF
 
 
-cat $RECORD_FILE
-RECORD_ID=$(laconic -c $CONFIG_FILE registry record publish --filename $RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
-echo $RECORD_ID
+cat $AR_RECORD_FILE
+AR_RECORD_ID=$(laconic -c $CONFIG_FILE registry record publish --filename $AR_RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
+echo $AR_RECORD_ID
 
 if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
   authority=$(echo "$rcd_name" | cut -d'/' -f1 | sed 's/@//')
   app=$(echo "$rcd_name" | cut -d'/' -f2-)
   CERC_REGISTRY_APP_CRN="lrn://$authority/applications/$app"
-  laconic registry authority reserve ${authority}
-  laconic registry authority bond set ${authority} ${CERC_REGISTRY_BOND_ID} --user-key "${CERC_REGISTRY_USER_KEY}"
+  laconic -c $CONFIG_FILE registry authority reserve ${authority} --user-key "${CERC_REGISTRY_USER_KEY}"
+  laconic -c $CONFIG_FILE registry authority bond set ${authority} ${CERC_REGISTRY_BOND_ID} --user-key "${CERC_REGISTRY_USER_KEY}"
 fi
 
-laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${rcd_app_version}" "$RECORD_ID"
-laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${CERC_REPO_REF}" "$RECORD_ID"
+laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${rcd_app_version}" "$AR_RECORD_ID"
+laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${CERC_REPO_REF}" "$AR_RECORD_ID"
 if [ "true" == "$CERC_IS_LATEST_RELEASE" ]; then
-  laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN" "$RECORD_ID"
+  laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN" "$AR_RECORD_ID"
 fi
 
-rm -f $RECORD_FILE $CONFIG_FILE
+
+APP_RECORD=$(laconic -c $CONFIG_FILE registry name resolve "$CERC_REGISTRY_APP_CRN" | jq '.[0]')
+if [ -z "$APP_RECORD" ] || [ "null" == "$APP_RECORD" ]; then
+  echo "No record found for $CERC_REGISTRY_APP_CRN."
+  exit 1
+fi
+
+cat <<EOF | sed '/.*: ""$/d' > "$ADR_RECORD_FILE"
+record:
+  type: ApplicationDeploymentRequest
+  version: 1.0.0
+  name: "$rcd_name@$rcd_app_version"
+  application: "$CERC_REGISTRY_APP_CRN@$rcd_app_version"
+  dns: "$CERC_REGISTRY_DEPLOYMENT_SHORT_HOSTNAME"
+  deployment: "$CERC_REGISTRY_DEPLOYMENT_CRN"
+  config:
+    env:
+      CERC_WEBAPP_DEBUG: "$rcd_app_version"
+  meta:
+    note: "Added by CI @ `date`"
+    repository: "`git remote get-url origin`"
+    repository_ref: "${GITHUB_SHA:-`git log -1 --format="%H"`}"
+EOF
+
+cat $ADR_RECORD_FILE
+ADR_RECORD_ID=$(laconic -c $CONFIG_FILE registry record publish --filename $ADR_RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
+echo $ADR_RECORD_ID
+
+rm -f $AR_RECORD_FILE $ADR_RECORD_FILE $CONFIG_FILE
