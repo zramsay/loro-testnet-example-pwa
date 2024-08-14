a764994df759dfb88f65d15563021247ebc9a3f0
zramsay <zach@bluecollarcoding.ca>
2024-08-14 15:00:26 -0400

    create bond and set name

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -27,6 +27,11 @@ services:
     fees: 15000000alnt
 EOF
 
+if [ -z "$CERC_REGISTRY_BOND_ID" ]; then
+  bond_id=$(laconic -c $CONFIG_FILE registry bond create --type alnt --quantity 1000000000000 --user-key "${CERC_REGISTRY_USER_KEY}")
+  CERC_REGISTRY_BOND_ID=$(echo ${bond_id} | jq -r .bondId)
+fi
+
 next_ver=$(laconic -c $CONFIG_FILE registry record list --type ApplicationRecord --all --name "$rcd_name" 2>/dev/null | jq -r -s ".[] | sort_by(.createTime) | reverse | [ .[] | select(.bondId == \"$CERC_REGISTRY_BOND_ID\") ] | .[0].attributes.version" | awk -F. -v OFS=. '{$NF += 1 ; print}')
 
 if [ -z "$next_ver" ] || [ "1" == "$next_ver" ]; then
@@ -58,6 +63,8 @@ if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
   authority=$(echo "$rcd_name" | cut -d'/' -f1 | sed 's/@//')
   app=$(echo "$rcd_name" | cut -d'/' -f2-)
   CERC_REGISTRY_APP_CRN="lrn://$authority/applications/$app"
+  laconic registry authority reserve ${authority}
+  laconic registry authority bond set ${authority} ${CERC_REGISTRY_BOND_ID} --user-key "${CERC_REGISTRY_USER_KEY}"
 fi
 
 laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${rcd_app_version}" "$RECORD_ID"
